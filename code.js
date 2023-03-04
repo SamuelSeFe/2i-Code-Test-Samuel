1287751735f7b87ff93ef3c0a33e36d4223a228e
SamuelSeFe <samuelsf@hotmail.co.uk>
2023-03-04 11:23:37 +0000

    Added method to remove duplicates
    Added the final method to remove duplicates and changed names of variables for easier reading. Also added notes to show my thinking and reasoning

--- code.js
+++ code.js
@@ -8,10 +8,22 @@ function sortUniqueNumberedList(numberedList) {
     return numberedList;
 
     // Now we sort the list (in descending order) using in-built methods in JS
-    arr = numberedList.sort(function (a,b) { return b - a; });
+    let array = numberedList.sort(function (a,b) { return b - a; });
 
-    return arr;
+  // We create a new list to add our unique numbers (we already add the first from our numbered list as this number will never be a duplicate itslef)  
+  let sortedList = [array[0]];
+    
+  // Now we can loop through each element in our sorted list of numbers starting from index 1
+    for (let i = 1; i < array.length; i++) {
+
+        // We can now compare through the loop any unique elements and add them to our new sortedList
+        if (array[i-1] !== array[i]) {
+            sortedList.push(array[i]);
+        }
+    }
+
+    return sortedList;
 }
 
 // Check to see if the sorting function works
-console.log(sortUniqueNumberedList([1,2,3,4,5]))
+console.log(sortUniqueNumberedList([45, 28, 39, 60, 85, 93, 24, 45, 60, 85, 24]))
